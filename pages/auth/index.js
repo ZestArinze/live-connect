04f598cded0070b7883d11cfe4e8bf45454c1478
Arinze Anakor <arinzeanakor@gmail.com>
2021-09-01 13:05:29 +0100

    chore(setup): add default files and blank pages

---
+++ pages/auth/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function index(props) {
+  return (
+    <>
+      <h1>Login</h1>
+    </>
+  );
+}
+
+export default index;
---
+++ pages/auth/register.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function register(props) {
+  return (
+    <>
+      <h1>Register</h1>
+    </>
+  );
+}
+
+export default register;
